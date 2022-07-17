54c7d5455a5ba2f2d6297389f9ae8e7aae7a009c
mato2 <mato2@umbc.edu>
2022-07-16 23:24:44 -0400

    updated resource name

--- asg-elb-service/dependencies.tf
+++ asg-elb-service/dependencies.tf
@@ -8,7 +8,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default" {
+data "aws_subnet" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
